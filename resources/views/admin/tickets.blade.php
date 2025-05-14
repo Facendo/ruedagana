2fab2d139e6937df97c2234c79e3446891ec1245
Francisco Hernandez <hernandezfrancisco672@gmail.com>
2025-05-13 21:24:25 -0400

    input de ticket funcional

--- resources/views/admin/tickets.blade.php
+++ resources/views/admin/tickets.blade.php
@@ -15,110 +15,99 @@
         <div class="section_tickets">
             <div>
                 <form action="{{route('ticket.store')}}" method="POST" class="form">
-                            @csrf
-                            @method('POST')
-                            <input type="hidden" name="id_sorteo" value="{{$sorteo->id_sorteo}}">
-                            <input type="hidden" name="cedula_cliente" value="{{$cliente->cedula}}">
-                            <input type="hidden" name="nombre_cliente" value="{{$cliente->nombre}}">
-                            <input type="hidden" name="telefono_cliente" value="{{$cliente->telefono}}">
-                            <input type="hidden" name="correo_cliente" value="{{$cliente->correo}}">
-                            <input type="hidden" name="id_pago" value="{{$pago->id_pago}}">
-                            <button>generar ticket</button>
+                    @csrf
+                    @method('POST')
+                    <input type="hidden" name="id_sorteo" value="{{$sorteo->id_sorteo}}">
+                    <input type="hidden" name="cedula_cliente" value="{{$cliente->cedula}}">
+                    <input type="hidden" name="nombre_cliente" value="{{$cliente->nombre}}">
+                    <input type="hidden" name="telefono_cliente" value="{{$cliente->telefono}}">
+                    <input type="hidden" name="correo_cliente" value="{{$cliente->correo}}">
+                    <input type="hidden" name="id_pago" value="{{$pago->id_pago}}">
+                    <input type="hidden" name="numeros_seleccionados" id="numeros_seleccionados">
+                    <button type="button" onclick="enviarTickets()">Generar Tickets Seleccionados</button>
                 </form>
             </div>
 
             <div class="container_tickets">
-                <h2 class="section_subtitle">Selecciona el ticket</h2>
-                <form action="" method="post" class="form_tickets">
+                <h2 class="section_subtitle">Selecciona los tickets</h2>
+                <form id="form_seleccionar_tickets" class="form_tickets">
                     <div class="numeros_tickets">
                         @csrf
-                            @php
-                                $numeros_disponibles = json_decode($sorteo->numeros_disponibles);
-                            @endphp
+                        @php
+                            $numeros_disponibles = json_decode($sorteo->numeros_disponibles);
+                        @endphp
                         @foreach ($numeros_disponibles as $numero )
-                            <form action="{{route('ticket.store')}}" method="POST" class="form">
-                            <input type="submit" name="name" value="{{$numero}}" class="button">
-                            
+                            <input type="checkbox" name="numeros[]" value="{{$numero}}" id="numero_{{$numero}}" class="input_checkbox">
+                            <label for="numero_{{$numero}}" class="button">{{$numero}}</label>
                         @endforeach
-
                     </div>
                 </form>
             </div>
 
-
-
             <div class="cont_tickets_selector">
-               
-            
-            <!------------- BLOQUEAR TICKETS ------------>
 
-            <h2 class="section_subtile">GESTIONA TICKETS</h2>
+                <h2 class="section_subtile">GESTIONA TICKETS</h2>
 
-            <h3 class="sub_inp">Bloquear Numero</h3>
-            <div class="cont_form">
-                <form action="{{route('ticket.bloquear',$sorteo)}}" class="form" method="POST" enctype="multipart/form-data">
-                    <h2>Bloquear Tickets</h2>
-                    @csrf
-                    @method('PUT')
-                    
-                    <input type="number" name="numero_a_bloquear" id="numero_a_bloquear" placeholder="Bloquear Numero" class="input_form" min="0" max="9999">
+                <h3 class="sub_inp">Bloquear Numero</h3>
+                <div class="cont_form">
+                    <form action="{{route('ticket.bloquear',$sorteo)}}" class="form" method="POST" enctype="multipart/form-data">
+                        <h2>Bloquear Tickets</h2>
+                        @csrf
+                        @method('PUT')
 
-                    
-                    <br>
+                        <input type="number" name="numero_a_bloquear" id="numero_a_bloquear" placeholder="Bloquear Numero" class="input_form" min="0" max="9999">
 
-                    <button type="submit" class="button">Bloquear Numero</button>
-                </form>
-            </div>
+                        <br>
 
-            <br><br><br><br><br><br><br><br><br><br>
+                        <button type="submit" class="button">Bloquear Numero</button>
+                    </form>
+                </div>
+
+                <br><br><br><br><br><br><br><br><br><br>
 
 
-            <!------------- DESBLOQUEAR CHECKLIST ------------>
-            
-            <div class="cont_form">
-                <form action="{{route('ticket.desbloquear',$sorteo)}}" class="form" method="post">
-                    @csrf
-                    @method('PUT')
-                    <h3 class="sub_inp">Desbloquear tickets</h3>
-                    <select name="numero_a_desbloquear" id="numero_a_desbloquear" class="input_select">
+                <div class="cont_form">
+                    <form action="{{route('ticket.desbloquear',$sorteo)}}" class="form" method="post">
+                        @csrf
+                        @method('PUT')
+                        <h3 class="sub_inp">Desbloquear tickets</h3>
+                        <select name="numero_a_desbloquear" id="numero_a_desbloquear" class="input_select">
                             @php
                                 $numeros_ganadores = json_decode($sorteo->numeros_ganadores);
                             @endphp
                             @if ($numeros_ganadores != null)
                                 @foreach($numeros_ganadores as $numero)
-                            <option value="{{$numero}}" class="input_option">{{$numero}}</option>
-                            @endforeach
+                                    <option value="{{$numero}}" class="input_option">{{$numero}}</option>
+                                @endforeach
+                            @else
+                                <option value="" class="input_option">No hay numeros ganadores</option>
                             @endif
-                        <option value="" class="input_option">No hay numeros ganadores</option>
-                    </select>
-                    <button type="submit" class="button">Desbloquear</button>
-                </form>
+                        </select>
+                        <button type="submit" class="button">Desbloquear</button>
+                    </form>
+                </div>
+
+                <br><br><br><br><br><br><br><br><br><br>
+
+
+                <div class="cont_form">
+                    <form action="#" class="form">
+                        <h3 class="sub_inp">Generar tickets aleatorios</h3>
+                        <button class="button">Generar</button>
+                    </form>
+                </div>
             </div>
-            
-            <br><br><br><br><br><br><br><br><br><br>    
-
-
-            <!--------- BOTTON PARA GENERACION ALEATORIA ------->
-
-            
-            <div class="cont_form">
-                <form action="#" class="form">
-                    <h3 class="sub_inp">Generar tickets aleatorios</h3>
-                    <button class="button">Generar</button>
-                </form>
-            </div>
-
-
-
-            </div>
-
-
-
-
         </div>
-
-
     </section>
-    
+
+    <script>
+        function enviarTickets() {
+            const checkboxes = document.querySelectorAll('input[name="numeros[]"]:checked');
+            const numerosSeleccionados = Array.from(checkboxes).map(checkbox => checkbox.value);
+            document.getElementById('numeros_seleccionados').value = JSON.stringify(numerosSeleccionados);
+            document.querySelector('form').submit();
+        }
+    </script>
+
 </body>
 </html>
