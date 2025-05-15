a01785a903b75735567e0b731d3c2016cddc6e1a
Francisco Hernandez <hernandezfrancisco672@gmail.com>
2025-05-14 20:29:35 -0400

    Funcionalidad de tickets completada

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -34,16 +34,30 @@ class TicketController extends Controller
      */
     public function store(Request $request)
     {   
-
-        $numerosSeleccionados = json_decode($request->numeros_seleccionados);
-
+        
         $sorteo = Sorteo::find($request->id_sorteo);
-
-        $numerosDisponibles = json_decode($sorteo->numeros_disponibles, true);
-
         $admin = User::all();
-
         $pago = Pago::find($request->id_pago);
+
+        
+        if($request->numeros_seleccionados == "aleatorio"){ 
+            $numerosSeleccionados = [];
+            $cantidad_a_seleccionar = $pago->cantidad_de_tickets;
+            $numerosDisponibles = json_decode($sorteo->numeros_disponibles, true);
+            $numerosSeleccionados = array_rand($numerosDisponibles, $cantidad_a_seleccionar);
+
+            // Asegurarse de que $numerosSeleccionados sea siempre un array
+            if (!is_array($numerosSeleccionados)) {
+                $numerosSeleccionados = [$numerosSeleccionados];
+            }
+        }
+
+        //Condicional si los numeros fueron seleccionados
+        else{
+            $numerosSeleccionados = json_decode($request->numeros_seleccionados, true);
+            $numerosDisponibles = json_decode($sorteo->numeros_disponibles, true);
+
+        }
         $pago->estado_pago = 'Confirmado';
         $pago->save();
 
@@ -74,7 +88,10 @@ class TicketController extends Controller
             Mail::to($user->email);
         }
         return redirect()->route('pago.index')->with('success', 'Ticket created successfully.');
+        
     }
+
+
 
     private function buildtoken($length = 8)
     {
@@ -150,8 +167,8 @@ class TicketController extends Controller
     {   
        
         $valorADesbloquear = $request->numero_a_desbloquear;
-        $numerosDisponibles = json_decode($sorteo->numeros_disponibles);
-        $numerosGanadores = json_decode($sorteo->numeros_ganadores);
+        $numerosDisponibles = json_decode($sorteo->numeros_disponibles,true);
+        $numerosGanadores = json_decode($sorteo->numeros_ganadores,true);
 
         $indiceEncontradoEnGanadores = array_search($valorADesbloquear, $numerosGanadores);
 
